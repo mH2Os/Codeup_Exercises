fa2e4e57eecb26463afc7799a2605376a66da410
Rissa Waters <rissa3896@yahoo.com>
2014-08-14 09:38:41 -0500

    new code

--- if.php
+++ if.php
@@ -1,47 +1,51 @@
 <?php
 
- $a = 5;
- $b = 10;
- $c = '10';
+$a=5;$b = 10; $c = '10';
+// Shorten the next 2 statements into an if/else
+if ($a < $b) {
+    // output the appropriate result
+    echo "$a is less than $b\n";
+} else {
+    echo "$a is greater than $b\n";
+}
 
- if ($a < $b) {
-     // output the appropriate result
-     echo "$a is less than $b\n";
- }
+if ($b > $a) {
+    // output the appropriate result
+    echo "$b is greater than $a\n";
+} else {
+    echo "$b is less than $a\n";
+}
 
- if ($b > $a) {
-     // output the appropriate result
-     echo "$b is greater than $a\n";
- }
+// Shorten the next 2 statements into an if/else
+if ($b >= $c) {
+    // output the appropriate result
+    echo "$b is greater than or equal to $c\n";
+} else {
+    echo "$b is less then or equal to $c\n";
+}
 
- if ($b >= $c) {
-     // output the appropriate result
-     echo "$b is greater than or equal to $c\n";
- }
+if ($b <= $c) {
+    // output the appropriate result
+    echo "$b is less than or equal to $c\n";
+} else {
+    echo "$b is greater than or equal to $c\n";
+}
 
- if ($b <= $c) {
-     // output the appropriate result
-     echo "$b is less than or equal to $c\n";
- }
+// combine the next 4 conditionals into 
+// one if/else/elseif block that checks in order for:
+// equal, identical, not equal, not identical
+if ($b == $c) {
+    // output the appropriate result
+    echo "$b is equal to $c\n";
+} elseif ($b === $c) {
+    // output the appropriate result
+    echo "$b is identical to $c\n";
+} elseif ($b != $c) {
+    // output the appropriate result
+    echo "$b is not equal to $c\n";
+} else {
+    // output the appropriate result
+    echo "$b is not identical to $c\n";
+}
 
- if ($b == $c) {
-     // output the appropriate result
-     echo "$b is equal to $c\n";
- }
-
- if ($b === $c) {
-     // output the appropriate result
-     echo "$b is  $c\n";
- }
-
- // output the appropriate result
- if ($b != $c) {
-     echo "$b is not equal to $c\n";
- }
-
- // output the appropriate result
- if ($b !== $c) {
-     echo "$b is not identical to $c\n";
- }
-
- ?>
+?>
